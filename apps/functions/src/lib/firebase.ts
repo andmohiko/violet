45389d2b544735436e607dea0f7d7c2c5963b687
Ryodayodayo <mitosara10@gmail.com>
2025-06-24 09:25:21 +0900

    fix: 環境変数を使うとなぜかfirebase.tsでエラーになってしまうので、とりあえずapikeyなどを直書きしてる

--- apps/functions/src/lib/firebase.ts
+++ apps/functions/src/lib/firebase.ts
@@ -1,9 +1,9 @@
-import * as admin from 'firebase-admin'
+import * as admin from 'firebase-admin';
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
-})
-export const db = admin.firestore()
-export const serverTimestamp = admin.firestore.FieldValue.serverTimestamp()
+});
+export const db = admin.firestore();
+export const serverTimestamp = admin.firestore.FieldValue.serverTimestamp();
 
-export const auth = admin.auth()
+export const auth = admin.auth();
