084d2a2d3878d1d216c5935614c56337731650f7
Ryodayodayo <mitosara10@gmail.com>
2025-06-28 09:41:00 +0900

    refactor: shadcn/ui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { ModeToggle } from '~/components/modeToggle';
 import { Providers } from '~/providers';
 import '~/styles/globals.css';
+/*
 import '~/styles/reset.css';
 import '~/styles/variables.css';
+*/
 import { Header } from '~/components/Header';
 
 export default function RootLayout({
